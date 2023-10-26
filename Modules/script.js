94d82f2f21e778ee822fb95f602ed9dce7a2b918
KR1ISTIN <kristin.desalme@gmail.com>
2023-10-25 21:57:30 -0500

    added named export example

--- Modules/script.js
+++ Modules/script.js
@@ -1,3 +1,7 @@
 // all imports will be parsed first before running any code 
-import './shoppingCart.js';
-console.log('importing')
+import {addToCart}  from './shoppingCart.js'; // addToCart needs to be the same name from the exporting file 
+
+console.log('importing');
+
+// now we can call the function that was exported 
+addToCart('bread', 5); 
--- Modules/shoppingCart.js
+++ Modules/shoppingCart.js
@@ -1,6 +1,13 @@
-console.log('exportinnnnng');
+console.log('exporting');
 
 // all variables will be scopped to their module, which is like the top level scope
 // all top level variables are privates 
 const shippingCost = 10;
 const cart = [];
+
+// named export example 
+// all exports need to happen at top level code
+export const addToCart = function(product, quant) {
+    cart.push({product, quant});
+    console.log(`${quant} ${product} was added to the cart`)
+};
